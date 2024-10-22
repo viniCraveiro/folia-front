e7cf918fe8321dc20f25d6fda735bf697cd8049b
Fernando <feres.angelo39@gmail.com>
2024-10-21 22:18:07 -0300

    feat: Dashboards e LocalStorage salvando a empresa

---
+++ src/pages/home/IBoletosData.ts
@@ -0,0 +1,6 @@
+export interface IBoletosData {
+    quantidadeBoletos: number;
+    quantidadeBoletosAberto: number;
+    quantidadeBoletosVencido: number;
+    quantidadeBoletosProximosVencimento: number;
+}
