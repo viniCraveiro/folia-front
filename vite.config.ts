45496728e3baeacde3d71ad304e4ed38b94b96f6
9asmiw <yasminmsilv4@gmail.com>
2024-11-11 17:54:26 -0300

    Dashboard de usuário e mudança na privateroute pra desviar do erro

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@mui/material/Tooltip', '@emotion/styled'],
+    include: ['@mui/material/Tooltip', '@emotion/styled', 'date-fns'],
   },
 })
