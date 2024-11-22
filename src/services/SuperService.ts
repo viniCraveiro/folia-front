0e0300a2f40ef54438a5f7a434c60ff41e81840c
Vinicius Craveiro <vinicius.craveiro@peon.moda>
2024-11-22 10:26:21 -0300

    Reajustando request para o backend

--- src/services/SuperService.ts
+++ src/services/SuperService.ts
@@ -4,7 +4,8 @@ import AuthService from "./AuthServices";
 const http = axios.create({
 });
 
-http.defaults.baseURL = "http://localhost:8080/api/" //URL PARA COMINUCACAO COM O A API
+http.defaults.baseURL = (location.origin).replace("5173", "8080") + "/api/"; //URL PARA COMINUCACAO COM O A API
+// http.defaults.baseURL = (location.origin+'/api/'); //URL PARA COMINUCACAO COM O A API
 
 http.defaults.headers.common['Content-Type'] = 'application/json';
 http.defaults.headers.common['Accept'] = 'application/json';
@@ -14,7 +15,7 @@ const authService = AuthService.getInstance();
 // Request Interceptor to add Authorization Token
 http.interceptors.request.use(
   (config) => {
-    const token = authService.getToken(); 
+    const token = authService.getToken();
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
@@ -26,10 +27,10 @@ http.interceptors.request.use(
 );
 
 // Response Interceptor for error handling
-http.interceptors.response.use( 
-  (response) =>  response,
+http.interceptors.response.use(
+  (response) => response,
   (error) => {
-    console.error(error)
+    console.error(error);
     const errorResponse = error.response?.data;
     return Promise.reject(errorResponse ?? error);
   }
